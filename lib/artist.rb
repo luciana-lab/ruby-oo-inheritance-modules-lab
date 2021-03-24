c8819d0022b281b474d8a636b41907d2c4b720ca
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-23 22:08:46 -0400

    commit tree 0a065eee1535a59e6bcf7f2e5fc2341763cb0ec3

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,42 @@
 require 'pry'
 
 class Artist
+
+  extend Findable::ClassMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    # self.class.all << self          -> before it was @@artist << self
+    super
     @songs = []
   end
+  #super: will execute any code placed inside the super class' method of the same name, and then execute any code inside the child class' method.
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  
+  #this method was taken out and added in the findable.rb
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #these both methods were taken out and added in the memorable.rb
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +47,9 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  ##this method was taken out and added in the paramable.rb
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,46 @@
 require 'pry'
 
 class Song
+
+  extend Findable::ClassMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   self.class.all << self          -> before it was @@songs << self
+  # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #this method was taken out and added in the findable.rb
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #these both methods were taken out and added in the memorable.rb
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #this method was taken out and added in the paramable.rb
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+
 end
